4342d718f7aacd8ca491bb646a89b5286c94daa0
Antonio SaIn <antoniosain@gmail.com>
2022-11-07 19:07:24 +0100

    commit inicial

---
+++ lib/screens/altas.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:iniestapruebas/screens/bajas.dart';
+
+class Altas extends StatelessWidget {
+  Altas({Key? key}) : super(key: key);
+  final iconosright = [
+    Icons.accessibility_new_outlined,
+    Icons.access_alarm_outlined,
+    Icons.accessibility_new,
+    Icons.accessible_sharp,
+  ];
+  final iconosleft = [
+    Icons.accessibility_new_outlined,
+    Icons.access_alarm_outlined,
+    Icons.accessibility_new,
+    Icons.accessible_sharp,
+  ];
+  final rutas = ['altas', 'altas', 'altas', 'altas'];
+  @override
+  Widget build(BuildContext context) {
+    final datos = ['ALTAS', 'BAJAS', 'MODIFICACIONES', 'ELIMINAR'];
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+                centerTitle: true, title: (const Text('MENU PRIjNCIPALl'))),
+            body: ListView.separated(
+                itemBuilder: (context, index) => ListTile(
+                      title: Text(datos[index]),
+                      trailing: Icon(iconosright[index]),
+                      leading: Icon(iconosleft[index]),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Bajas(),
+                            ));
+                      },
+                    ),
+                separatorBuilder: (_, __) => const Divider(),
+                itemCount: datos.length)));
+  }
+}
